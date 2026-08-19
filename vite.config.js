bb9c82434d867dec3bd39b21b6f08d90a833d02a
aria <aria.chalipa.2002@gmail.com>
2026-08-19 19:44:09 +0330

    adding code needed for creating github pages

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-    vueDevTools(),
-  ],
+  base: '/daily-management-system/',
+  plugins: [vue(), tailwindcss(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
